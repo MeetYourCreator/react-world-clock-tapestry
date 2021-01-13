09716fbe6372fa29d765840025a6a057f5ab6160
MeetYourCreator <ramizackshamir@icloud.com>
2021-01-13 11:24:38 -0500

    local time set, useEffect to make clock tick forward every 1000 ms

--- src/Components/LocalClock/LocalClock.jsx
+++ src/Components/LocalClock/LocalClock.jsx
@@ -2,8 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { showTime } from '../../showTime.js'
 
 const LocalClock = () => {
-
+const [localTime, setLocalTime] = useState(showTime())
   
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLocalTime(showTime())
+    }, 1000)
+  
+  }) 
+
   return (
     <>
     </>
--- src/showTime.js
+++ src/showTime.js
@@ -3,15 +3,23 @@ export const showTime = () => {
   let time = new Date();
   let hour = time.getHours();
   let minute = time.getMinutes();
-  let second = time.getSecond();
-
+  let second = time.getSeconds();
   let am_pm = 'AM';
+
   if (hour >= 12) {
     hour -= 12;
     let am_pm = 'PM'
   } else {
     let am_pm = 'AM'
   }
+
+  hour = hour < 10 ? '0' + hour : hour;
+  minute = minute < 10 ? '0' + minute : minute;
+  second = second < 10 ? '0' + second : second;
+
+  let currentTime = hour + ':' + minute + ':' + second + am_pm
+
+  return currentTime
 
 }
 
