a4377fb2dc1273eaa4e4fc600eb13e55b84ddd0a
MeetYourCreator <ramizackshamir@icloud.com>
2021-01-13 10:19:12 -0500

    init file architecture for app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import "./App.css"
+import LocalClock from "./Components/LocalClock/LocalClock.jsx"
+
+const App = () => {
+
+  return (
+    <>
+      <LocalClock />
+    </>
+)
+}
+
+export default App;
+
