33aece9d21d88ffb9ebbac765b71f0c9cdc9cf44
MeetYourCreator <ramizackshamir@icloud.com>
2021-01-14 12:27:38 -0500

    api data renders to grid for each entry

--- src/Components/WorldClock/WorldClock.jsx
+++ src/Components/WorldClock/WorldClock.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { v4 as uuid4 } from 'uuid'
+import './WorldClock.css'
 
 const WorldClock = () => {
   const [apiData, setApiData] = useState([])
+  const [worldTime, setWorldTime] = useState({})
   console.log(apiData)
   const url = `http://api.timezonedb.com/v2.1/list-time-zone?key=7KJHTP7QS6J7&format=json`
  
+
+  console.log(`WorldTime: ${worldTime}`)
+
 
 useEffect(() => {
    const makeApiCall = async () => {
@@ -17,15 +22,41 @@ useEffect(() => {
    makeApiCall()
  }, [])
 
+  useEffect(() => {
+    setTimeout(() => {
+    setWorldTime(showWorldTime)
+    }, 2000)
+  }, [])
+
+ const showWorldTime = () => {
+   let time = new Date()
+   console.log(`Time: ${time}`)
+    let hour = time.setHours()
+    let minute = time.setMinutes()
+    let second = time.setSeconds()
+    let am_pm = "AM"
+
+    hour = hour < 10 ? "0" + hour : hour
+    minute = minute < 10 ? "0" + minute : minute
+    second = second < 10 ? "0" + second : second
+
+    let wt= hour + ":" + minute + ":" + second + am_pm
+  console.log(wt)
+    return wt
+ }
+  
+  console.log(showWorldTime.time)
+  showWorldTime()
   return (
     <>
-      <div>
+
         {apiData.map((clock) =>
           // console.table(clock)
-          <div key={uuid4()}>{clock.countryCode}
+          <div key={uuid4()} className='world-time-container'>
+            <span className='world-clock'>{clock.timestamp}</span>
+            <p className='world-time-text'>{clock.zoneName}</p>
           </div>
         )}
-      </div>
     </>
   )
 }
