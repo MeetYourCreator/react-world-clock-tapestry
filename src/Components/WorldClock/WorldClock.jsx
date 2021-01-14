0b26e809cc04f83a282d61c589a12e14320e9837
MeetYourCreator <ramizackshamir@icloud.com>
2021-01-14 09:37:15 -0500

    replaced API

--- src/Components/WorldClock/WorldClock.jsx
+++ src/Components/WorldClock/WorldClock.jsx
@@ -4,24 +4,23 @@ import axios from 'axios'
 const WorldClock = () => {
   const [apiData, setApiData] = useState('')
 console.log(apiData)
-  const url = `http://worldtimeapi.org/api/timezone/`
+  const url = `http://api.timezonedb.com/v2.1/list-time-zone?key=7KJHTP7QS6J7&format=json`
  
 
   useEffect(() => {
     const makeApiCall = async () => {
       const data = await axios.get(url)
-      console.log(data.data)
-      setApiData(data.data)
+      
+      setApiData(data.data.zones)
     }
     makeApiCall();
   }, [])
 
   return (
     <>
-  
+      <div>{apiData.map(clock => console.table(clock))}</div>
     </>
-    
-)
+  )
 }
 
 export default WorldClock;
