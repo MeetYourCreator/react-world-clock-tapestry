064e9e02dd60b1eb2e94426155d9dea1955a187d
MeetYourCreator <ramizackshamir@icloud.com>
2021-01-14 13:33:31 -0500

    timestamp prop, prior to Component refactor

--- src/Components/WorldClock/WorldClock.jsx
+++ src/Components/WorldClock/WorldClock.jsx
@@ -6,7 +6,9 @@ import './WorldClock.css'
 const WorldClock = () => {
   const [apiData, setApiData] = useState([])
   const [worldTime, setWorldTime] = useState({})
-  console.log(apiData)
+
+ 
+  console.log()
   const url = `http://api.timezonedb.com/v2.1/list-time-zone?key=7KJHTP7QS6J7&format=json`
  
 
@@ -18,45 +20,33 @@ useEffect(() => {
      const data = await axios.get(url)
 
      setApiData(data.data.zones)
+     console.log(data.data.zones)
    }
    makeApiCall()
  }, [])
 
   useEffect(() => {
     setTimeout(() => {
-    setWorldTime(showWorldTime)
+      setWorldTime(worldTime)
     }, 2000)
   }, [])
 
- const showWorldTime = () => {
-   let time = new Date()
-   console.log(`Time: ${time}`)
-    let hour = time.setHours()
-    let minute = time.setMinutes()
-    let second = time.setSeconds()
-    let am_pm = "AM"
-
-    hour = hour < 10 ? "0" + hour : hour
-    minute = minute < 10 ? "0" + minute : minute
-    second = second < 10 ? "0" + second : second
-
-    let wt= hour + ":" + minute + ":" + second + am_pm
-  console.log(wt)
-    return wt
- }
-  
-  console.log(showWorldTime.time)
-  showWorldTime()
+ 
+ 
+ 
   return (
     <>
-
-        {apiData.map((clock) =>
-          // console.table(clock)
-          <div key={uuid4()} className='world-time-container'>
-            <span className='world-clock'>{clock.timestamp}</span>
-            <p className='world-time-text'>{clock.zoneName}</p>
-          </div>
-        )}
+      {apiData.map((clock) => (
+        // console.table(clock)
+        <div
+          key={uuid4()}
+          className="world-time-container"
+          timestamp={clock.timestamp}
+        >
+          <span className="world-clock">{clock.timestamp}</span>
+          <p className="world-time-text">{clock.zoneName}</p>
+        </div>
+      ))}
     </>
   )
 }
