942d882a56b2c203a4f819895ec1adc80c615992
MeetYourCreator <ramizackshamir@icloud.com>
2021-01-15 10:55:31 -0500

    import moment.js

--- src/Components/WorldClockChild/WorldClockChild.jsx
+++ src/Components/WorldClockChild/WorldClockChild.jsx
@@ -1,28 +1,44 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import moment from 'moment'
 import './WorldClockChild.css'
 
 const WorldClockChild = ({id, timestamp, zonename}) => {
-  const [worldTime, setworldtime] = useState(timestamp)
+  const [worldTime, setWorldTime] = useState('')
   
   console.log(`WorldTime: ${worldTime}`)
 
 
-const showWorldTime = () => {
-  let date = new Date(timestamp)
-  let time = date.toTimeString()
-  // let hour = time.getHours()
-  // let minute = time.getMinutes()
-  // let second = time.getSeconds()
-
-  return time
-}
-  
-
   useEffect(() => {
     setTimeout(() => {
-      setworldtime(worldTime)
+      setWorldTime(showWorldTime())
     }, 1000)
   }, []) 
+  
+const showWorldTime = () => {
+  let time = new Date(moment(timestamp))
+  let hour = time.getHours()
+  let minute = time.getMinutes()
+  let second = time.getSeconds()
+  let am_pm = "AM"
+
+  if (hour > 12) {
+    hour -= 12
+    am_pm = "PM"
+  } else {
+    am_pm = "AM"
+  }
+
+  hour = hour < 10 ? "0" + hour : hour
+  minute = minute < 10 ? "0" + minute : minute
+  second = second < 10 ? "0" + second : second
+
+  let currentTime = hour + ":" + minute + ":" + second + am_pm
+
+  return currentTime
+}
+
+  
+
 
   return (
     <>
