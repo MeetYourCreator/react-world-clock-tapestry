ae0b9aa3e9430ebee9ac5e6d3ce34291328bdd67
MeetYourCreator <ramizackshamir@icloud.com>
2021-01-15 11:18:19 -0500

    correct time format renders to broswer

--- src/Components/WorldClockChild/WorldClockChild.jsx
+++ src/Components/WorldClockChild/WorldClockChild.jsx
@@ -1,49 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import moment from 'moment'
 import './WorldClockChild.css'
+import { showWorldTime } from "../../services/showWorldTime.js"
 
 const WorldClockChild = ({id, timestamp, zonename}) => {
-  const [worldTime, setWorldTime] = useState('')
+  const [worldTime, setWorldTime] = useState(showWorldTime(timestamp))
   
   console.log(`WorldTime: ${worldTime}`)
+  console.log(`timestamp: ${timestamp}`)
 
 
   useEffect(() => {
     setTimeout(() => {
-      setWorldTime(showWorldTime())
+      setWorldTime(worldTime)
     }, 1000)
   }, []) 
-
-const showWorldTime = () => {
-  let time = new Date(moment(timestamp))
-  let hour = time.getHours()
-  let minute = time.getMinutes()
-  let second = time.getSeconds()
-  let am_pm = "AM"
-
-  if (hour > 12) {
-    hour -= 12
-    am_pm = "PM"
-  } else {
-    am_pm = "AM"
-  }
-
-  hour = hour < 10 ? "0" + hour : hour
-  minute = minute < 10 ? "0" + minute : minute
-  second = second < 10 ? "0" + second : second
-
-  let currentTime = hour + ":" + minute + ":" + second + am_pm
-
-  return currentTime
-}
-
-  
 
 
   return (
     <>
       <div id={id} className="world-time-container" >
-        <span className="world-clock">{timestamp}</span>
+        <span className="world-clock">{worldTime}</span>
         <p className="world-time-text">{zonename}</p>
       </div>
     </>
---
+++ src/services/showWorldTime.js
@@ -0,0 +1,22 @@
+export const showWorldTime = (t) => {
+  let time = new Date(t)
+  let hour = time.getHours()
+  let minute = time.getMinutes()
+  let second = time.getSeconds()
+  let am_pm = "AM"
+
+  if (hour > 12) {
+    hour -= 12
+    am_pm = "PM"
+  } else {
+    am_pm = "AM"
+  }
+
+  hour = hour < 10 ? "0" + hour : hour
+  minute = minute < 10 ? "0" + minute : minute
+  second = second < 10 ? "0" + second : second
+
+  let currentTime = hour + ":" + minute + ":" + second + am_pm
+
+  return currentTime
+}
