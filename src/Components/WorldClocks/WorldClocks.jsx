4289b2c878ef6ceb2605e1d36a4bcffcb10ad8a1
MeetYourCreator <ramizackshamir@icloud.com>
2021-01-21 18:40:24 -0500

    destructure props in WorldClocks Component

--- src/Components/WorldClocks/WorldClocks.jsx
+++ src/Components/WorldClocks/WorldClocks.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import WorldClock from '../WorldClock/WorldClock.jsx'
 import { v4 as uuid4 } from "uuid"
-const WorldClocks = (props) => {
-
-  const { allTimeZones, worldtime } = props
-  
- 
+const WorldClocks = ({ allTimeZones, worldtime }) => {
 
   return (
     <>
