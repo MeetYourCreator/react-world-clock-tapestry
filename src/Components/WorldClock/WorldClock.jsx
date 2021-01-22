17870adbb735895484ecadd4a11db0dc5e13cbd7
MeetYourCreator <ramizackshamir@icloud.com>
2021-01-22 14:58:21 -0500

    unix and gmtoffset props returned

--- src/Components/WorldClock/WorldClock.jsx
+++ src/Components/WorldClock/WorldClock.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import {showWorldTime } from '../../services/time.js'
 import './WorldClock.css'
 
-const WorldClock = (id, {unix, gmtoffset, zonename}) => {
-  let time = unix - gmtoffset
+const WorldClock = ({id, unix, gmtoffset, zonename}) => {
+  // let time = unix - gmtoffset
   
   return (
     <>
       <div id={id} className={`world-clock-container`}>
         <p className="world-clock">
-          <span className="unix-timestamp">{time}</span>
-          {/* <span className='gmtOffset'>{unix}</span> */}
+          <span className="unix-timestamp">{unix}</span>
+          <span className='gmtoffset'>{gmtoffset}</span>
         </p>
         <p className="world-time-text">{zonename}</p>
       </div>
