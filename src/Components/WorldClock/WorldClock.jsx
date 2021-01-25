e08a157fe6129dd7ee240fa12c646005e3b674e8
MeetYourCreator <ramizackshamir@icloud.com>
2021-01-25 09:47:00 -0500

    psseudocode WorldClock.jsx

--- src/Components/WorldClock/WorldClock.jsx
+++ src/Components/WorldClock/WorldClock.jsx
@@ -7,10 +7,13 @@ const WorldClock = ({id, unix, className, gmtoffset, zonename}) => {
   return (
     <>
       <div id={id} className={className}>
-        <button onClick={() => {
-          let utc = unix - gmtoffset;
-          alert(utc)
-        }}></button>
+        <button
+          //NOTE: When the button is clicked the correct difference is alerted. BUT EXECUTING AN ALERT DOES NOT DOES NOT CHANGE STATE. That is why the correct difference can be alerted on an onClcik, when props are immutable in Child Components.
+          onClick={() => {
+            let utc = unix - gmtoffset
+            alert(utc)
+          }}
+        ></button>
         <p className="world-clock">
           <span className="unix-timestamp">{unix}</span>
           <span className="gmtoffset">{gmtoffset}</span>
