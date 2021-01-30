0fb7c98e0a19aa7eb78dbc2f36d36cb9e26da23d
MeetYourCreator <ramizackshamir@icloud.com>
2021-01-30 14:48:06 -0500

    fixed formatZoneName export

--- src/Components/WorldClock/WorldClock.jsx
+++ src/Components/WorldClock/WorldClock.jsx
@@ -3,14 +3,20 @@ import styled from "styled-components"
 import "./WorldClock.css"
 
 const WorldClock = ({ id, unix, className, zonename, country }) => {
+  
+
   return (
     <>
-      <div id={id} className={className}>
-        <p className="world-clock">
-          <span className="unix-timestamp">{unix}</span>
-        </p>
-        <p className="world-time-text">{zonename}, {country}</p>
-      </div>
+      {country !== zonename && (
+        <div id={id} className={className}>
+          <p className="world-clock">
+            <span className="unix-timestamp">{unix}</span>
+          </p>
+            <p className="world-time-text">
+              {zonename}, {country}
+            </p>
+        </div>
+      )}
     </>
   )
 }
--- src/services/time.js
+++ src/services/time.js
@@ -30,23 +30,39 @@ export const showLocalTime = () => {
 
 }
 
-export const showISO = (timestampMS) => {
+export const showISO = (timestampMS, styleLight, styleDark) => {
   const zoneISO = new Date(timestampMS * 1000).toISOString();
   const zoneISOhhmmss = zoneISO.slice(11, 19)
   return zoneISOhhmmss
 }
 
-export const formatZoneName = (zone) => {
+export const formatZoneName = (zoneStr) => {
   
-  const zoneArr = zone.split('')
-  const indexOfForwardSlash = zoneArr.findIndex((character) => character === '/')
+  console.log(`time.js, 41 zoneStr: ${zoneStr}`)
 
-  const zoneArr2 = zone.split(',')
-  const nameToString = zoneArr2.toString()
-  
-  const country = nameToString.slice(indexOfForwardSlash).replace('/', '').replace('_', ' ').replace('_', '  ')
+  const zoneArr = zoneStr.split('')
+  console.log(`time.js, 44 zoneArr: ${zoneArr}`)
 
+  const indexOfFirstForwardSlash = zoneArr.findIndex((firstForwardSlash) => firstForwardSlash === '/')
+  console.log(`time.js, 47 indexOfFirstForwardSlash: ${indexOfFirstForwardSlash}`)
 
-  return country
+  const zoneArr2 = zoneStr.split(',')
+  console.log(`time.js, 50 zoneArr2: ${zoneArr2}`)
 
+  const zoneStr2 = zoneArr2.toString()
+  console.log(`time.js, 53 zoneStr2: ${zoneStr2}`)
+
+  const firstForwardSlashRemovedArr = zoneStr2.slice(indexOfFirstForwardSlash).replace('/', '')
+  console.log(`time.js, 56 firstForwardSlashRemovedArr: ${firstForwardSlashRemovedArr}`)
+
+  const indexOfSecondForwardSlash = firstForwardSlashRemovedArr.split('').findIndex((secondForwardSlash) => secondForwardSlash === '/')
+  console.log(`time.js, 59 indexOfSecondForwardSlash: ${indexOfSecondForwardSlash}`)
+
+  if (indexOfSecondForwardSlash >= 0) {
+    const country1 = firstForwardSlashRemovedArr.slice(indexOfSecondForwardSlash).replace('/', ' ').replace('_', '  ').replace('_', '  ');
+    return country1
+  } else {
+    const country2 = firstForwardSlashRemovedArr.replace('_', ' ').replace('_', ' ')
+    return country2
+  }
 }
