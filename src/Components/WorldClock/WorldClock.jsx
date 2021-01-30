e4a21df4fbd07143959b76baa2a466d333534be7
MeetYourCreator <ramizackshamir@icloud.com>
2021-01-30 16:01:43 -0500

    styled components init for WorldClock.jsx

--- src/Components/WorldClock/WorldClock.jsx
+++ src/Components/WorldClock/WorldClock.jsx
@@ -2,20 +2,50 @@ import React from "react"
 import styled from "styled-components"
 import "./WorldClock.css"
 
-const WorldClock = ({ id, unix, className, zonename, country }) => {
+const ClockContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-top: 2px solid #fac931;
+  border-right: 2px solid #fac931;
+  border-bottom: 2px solid #fac931;
+  border-left: 2px solid #fac931;
+  padding-top: 30px;
+  font-family: "Pangolin", cursive;
+  font-size: 18px;
+`
+const Location = styled.p`
+  margin-bottom: 5px;
+  margin-left: 7px;
+  margin-right: 7px;
+  border-top: 2px solid black;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  color: black;
+  font-size: 11px;
+  font-family: "Noto Sans TC", sans-serif;
+`
+
+const Clock = styled.p`
+  color: #ff0fd7;
+`
+const WorldClock = ({ id, unix, zonename, country }) => {
   
 
   return (
     <>
       {country !== zonename && (
-        <div id={id} className={className}>
-          <p className="world-clock">
-            <span className="unix-timestamp">{unix}</span>
-          </p>
-            <p className="world-time-text">
-              {zonename}, {country}
-            </p>
-        </div>
+        <ClockContainer>
+          <Clock>
+            {unix}
+          </Clock>
+          <Location>
+            {zonename}, {country}
+          </Location>
+        </ClockContainer>
       )}
     </>
   )
