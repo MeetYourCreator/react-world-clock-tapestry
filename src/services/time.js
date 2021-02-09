426a6c48de75628937efb67e8cf1593b56067d59
MeetYourCreator <ramizackshamir@icloud.com>
2021-02-09 09:45:16 -0500

    code cleanup

--- src/services/time.js
+++ src/services/time.js
@@ -13,15 +13,7 @@ export const showLocalTime = () => {
   let hour = time.getHours();
   let minute = time.getMinutes();
   let second = time.getSeconds();
-  // let am_pm = 'AM';
-
-  // if (hour > 12) {
-  //   hour -= 12;
-  //   am_pm = 'PM'
-  // } else {
-  //   am_pm = 'AM'
-  // }
-
+  
   hour = hour < 10 ? '0' + hour : hour;
   minute = minute < 10 ? '0' + minute : minute;
   second = second < 10 ? '0' + second : second;
